6799afae8506cd3da50ca4730e0c0d73115b376b
Justin Walsh <contact.me@thejustinwalsh.com>
2021-11-19 04:09:35 -0700

    refactor: more interesting game progression

--- src/scripts/walls.script.ts
+++ src/scripts/walls.script.ts
@@ -4,22 +4,52 @@ type Message = {
 };
 
 interface props {
-  speed: number;
+  // tunable properties
   wall_count: number;
+  starting_speed: number;
+  starting_delay: number;
+  minimum_delay: number;
+  delay_rate: number;
+  difficulty: number;
+  seed: number;
+  // member props
+  speed: number;
   walls: hash[];
   active_walls: hash[];
   dead_walls: hash[];
   triggered_walls: Set<url>;
+  wall_delay: number;
+  walls_spawned: number;
+  max: number;
+  delay: number;
+  distance: number;
+  epoch: number;
 }
 
-go.property("speed", 60);
-go.property("wall_count", 1);
+go.property("wall_count", 0);
+go.property("starting_speed", 90);
+go.property("starting_delay", 6);
+go.property("minimum_delay", 1.5);
+go.property("delay_rate", 0.05);
+go.property("difficulty", 10);
+go.property("seed", 4277009102);
+
+const DISTANCE = 640 + 16;
 
 export function init(this: props): void {
   this.walls = [];
   this.active_walls = [];
   this.dead_walls = [];
   this.triggered_walls = new Set();
+  this.wall_delay = 0;
+  this.walls_spawned = 1;
+
+  this.max = 1;
+  this.speed = this.starting_speed;
+  this.delay = this.starting_delay;
+  this.distance = DISTANCE;
+  this.epoch = socket.gettime();
+  math.randomseed(this.seed);
 
   for (let i = 0; i < this.wall_count; i++) {
     const wall_id = `/${i + 1}`;
@@ -29,18 +59,26 @@ export function init(this: props): void {
 }
 
 export function update(this: props, dt: number): void {
+  // Update progression
+  update_progression(this, socket.gettime() - this.epoch);
+  if (this.wall_delay >= 0) this.wall_delay -= dt;
+  
+  // Add dead walls back to the pool
   for (const wall of this.dead_walls.splice(0, this.dead_walls.length)) {
     this.triggered_walls.delete(wall);
     this.walls.push(wall);
   }
 
-  if (this.active_walls.length <= 0) {
-    const wall = this.walls.shift();
-    if (wall) {
-      this.active_walls.push(wall);
-    }
+  // Spawn walls
+  if (this.wall_delay <= 0 && this.walls.length > 0) {
+    const wall = this.walls.splice(math.random(0, this.walls.length - 1), 1)[0];
+    print(wall, this.wall_delay, this.speed);
+    go.set_position(vmath.vector3(0, 0, 0), wall);
+    this.active_walls.push(wall);
+    this.wall_delay = this.delay;
   }
 
+  // Update walls
   for (let i = 0; i < this.active_walls.length; i++) {
     const wall = this.active_walls[i];
     const pos = go.get_position(wall);
@@ -49,6 +87,7 @@ export function update(this: props, dt: number): void {
       this.active_walls.splice(i, 1);
       this.dead_walls.push(wall);
       go.set_position(vmath.vector3(0, 0, 0), wall);
+      this.walls_spawned++;
     } else {
       go.set_position(pos, wall);
     }
@@ -66,4 +105,14 @@ export function on_message(
       msg.post("/gui#hud", "score", { score: 1 });
     }
   }
+}
+
+function update_progression(props: props, elapsed: number): void {
+  if (props.walls_spawned % props.difficulty === 0) {
+    props.delay = math.max(props.minimum_delay, props.starting_delay - props.walls_spawned * props.delay_rate);
+  }
+  if (props.walls_spawned > 0 && props.difficulty === 0) {
+    props.difficulty = math.floor(props.difficulty / 2);
+  }
+  props.speed = props.starting_speed + elapsed * 0.1;
 }
