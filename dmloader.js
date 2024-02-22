cd01f11dd8da2d939ea55996e0bd6117ab1d369b
thinknathan <github@thinknathan.com>
2024-02-22 04:36:01 +0000

    Deploying to gh-pages from @ ts-defold/tsd-template-yagames@af6372663bd2dac00a27b95087be4f998ee1b907 🚀

--- dmloader.js
+++ dmloader.js
@@ -107,32 +107,33 @@ var EngineLoader = {
 
     stream_wasm: false,
 
-    loadAndInstantiateWasmAsync: function(src, fromProgress, toProgress, callback) {
+    // load and instantiate .wasm file using XMLHttpRequest
+    loadAndInstantiateWasmAsync: function(src, fromProgress, toProgress, imports, successCallback) {
         FileLoader.load(src, "arraybuffer", EngineLoader.wasm_size,
             function(loaded, total) { Progress.calculateProgress(fromProgress, toProgress, loaded, total); },
             function(error) { throw error; },
             function(wasm) {
-                Module.instantiateWasm = function(imports, successCallback) {
-                    var wasmInstantiate = WebAssembly.instantiate(new Uint8Array(wasm), imports).then(function(output) {
-                        successCallback(output.instance);
-                    }).catch(function(e) {
-                        console.log('wasm instantiation failed! ' + e);
-                        throw e;
-                    });
-                    return {}; // Compiling asynchronously, no exports.
-                }
-                callback();
+                var wasmInstantiate = WebAssembly.instantiate(new Uint8Array(wasm), imports).then(function(output) {
+                    successCallback(output.instance);
+                }).catch(function(e) {
+                    console.log('wasm instantiation failed! ' + e);
+                    throw e;
+                });
             });
     },
 
-    setupWasmStreamAsync: async function(src, fromProgress, toProgress) {
+    // stream and instantiate .wasm file
+    streamAndInstantiateWasmAsync: async function(src, fromProgress, toProgress, imports, successCallback) {
         // https://stackoverflow.com/a/69179454
         var fetchFn = fetch;
         if (typeof TransformStream === "function" && ReadableStream.prototype.pipeThrough) {
             async function fetchWithProgress(path) {
                 const response = await fetch(path);
                 // May be incorrect if compressed
-                const contentLength = response.headers.get("Content-Length");
+                var contentLength = response.headers.get("Content-Length");
+                if (!contentLength){
+                    contentLength = EngineLoader.wasm_size;
+                }
                 const total = parseInt(contentLength, 10);
 
                 let bytesLoaded = 0;
@@ -140,7 +141,7 @@ var EngineLoader = {
                     transform (chunk, controller) {
                         bytesLoaded += chunk.byteLength;
                         Progress.calculateProgress(fromProgress, toProgress, bytesLoaded, total);
-                        controller.enqueue(chunk)
+                        controller.enqueue(chunk);
                     }
                 });
 
@@ -149,30 +150,29 @@ var EngineLoader = {
             fetchFn = fetchWithProgress;
         }
 
-        Module.instantiateWasm = function(imports, successCallback) {
-            WebAssembly.instantiateStreaming(fetchFn(src), imports).then(function(output) {
-                Progress.calculateProgress(fromProgress, toProgress, 1, 1);
-                successCallback(output.instance);
-            }).catch(function(e) {
-                console.log('wasm streaming instantiation failed! ' + e);
-                throw e;
-            });
-            return {}; // Compiling asynchronously, no exports.
-        }
+        WebAssembly.instantiateStreaming(fetchFn(src), imports).then(function(output) {
+            Progress.calculateProgress(fromProgress, toProgress, 1, 1);
+            successCallback(output.instance);
+        }).catch(function(e) {
+            console.log('wasm streaming instantiation failed! ' + e);
+            console.log('Fallback to wasm loading');
+            EngineLoader.loadAndInstantiateWasmAsync(src, fromProgress, toProgress, imports, successCallback);
+        });
     },
 
     // instantiate the .wasm file either by streaming it or first loading and then instantiate it
-    // https://github.com/emscripten-core/emscripten/blob/master/tests/manual_wasm_instantiate.html#L170
+    // https://github.com/emscripten-core/emscripten/blob/main/test/manual_wasm_instantiate.html
     loadWasmAsync: function(exeName) {
-        if (EngineLoader.stream_wasm && (typeof WebAssembly.instantiateStreaming === "function")) {
-            EngineLoader.setupWasmStreamAsync(exeName + ".wasm", 10, 50);
-            EngineLoader.loadAndRunScriptAsync(exeName + '_wasm.js', EngineLoader.wasmjs_size, 0, 10);
-        }
-        else {
-            EngineLoader.loadAndInstantiateWasmAsync(exeName + ".wasm", 0, 40, function() {
-                EngineLoader.loadAndRunScriptAsync(exeName + '_wasm.js', EngineLoader.wasmjs_size, 40, 50);
-            });
-        }
+        Module.instantiateWasm = function(imports, successCallback) {
+            if (EngineLoader.stream_wasm && (typeof WebAssembly.instantiateStreaming === "function")) {
+                EngineLoader.streamAndInstantiateWasmAsync(exeName + ".wasm", 10, 50, imports, successCallback);
+            }
+            else {
+                EngineLoader.loadAndInstantiateWasmAsync(exeName + ".wasm", 10, 50, imports, successCallback);
+            }
+            return {}; // Compiling asynchronously, no exports.
+        };
+        EngineLoader.loadAndRunScriptAsync(exeName + '_wasm.js', EngineLoader.wasmjs_size, 0, 10);
     },
 
     loadAsmJsAsync: function(exeName) {
@@ -479,7 +479,7 @@ var Progress = {
 
     updateProgress: function(percentage) {
         if (Progress.bar) {
-            Progress.bar.style.width = percentage + "%";
+            Progress.bar.style.width = Math.min(percentage, 100) + "%";
         }
         Progress.notifyListeners(percentage);
     },
@@ -725,7 +725,7 @@ var Module = {
         FS.syncfs(true, function(err) {
             if (err) {
                 Module._syncTries += 1;
-                console.error("FS syncfs error: " + err);
+                console.warn("Unable to synchronize mounted file systems: " + err);
                 if (Module._syncMaxTries > Module._syncTries) {
                     Module.preSync(done);
                 } else {
@@ -847,7 +847,7 @@ var Module = {
                 Module._syncInProgress = false;
 
                 if (err) {
-                    console.error("Module._startSyncFS error: " + err);
+                    console.warn("Unable to synchronize mounted file systems: " + err);
                     Module._syncTries += 1;
                 }
 
