49ed1f3053e7f95f8b9097bd83c75913e9a0c891
thejustinwalsh <contact.me@thejustinwalsh.com>
2024-06-29 18:11:26 +0000

    Deploying to gh-pages from @ ts-defold/tsd-template-yagames@f6ccca0416191cb9c592032536af9d05c999b339 🚀

--- dmloader.js
+++ dmloader.js
@@ -49,10 +49,6 @@ var CUSTOM_PARAMETERS = {
         var prevInnerWidth = -1;
         var prevInnerHeight = -1;
         
-        buttonHeight = 42;
-        
-        
-        buttonHeight = 42;
         
         // Hack for iOS when exit from Fullscreen mode
         if (is_iOS) {
@@ -74,28 +70,16 @@ var CUSTOM_PARAMETERS = {
         var targetRatio = width / height;
         var actualRatio = innerWidth / innerHeight;
     
-        //Downscale fit
-        if (innerWidth < width || innerHeight < height) {
-            if (actualRatio > targetRatio) {
-                width = innerHeight * targetRatio;
-                height = innerHeight;
-                app_container.style.marginLeft = ((innerWidth - width) / 2) + "px";
-                app_container.style.marginTop = "0px";
-            }
-            else {
-                width = innerWidth;
-                height = innerWidth / targetRatio;
-                app_container.style.marginLeft = "0px";
-                app_container.style.marginTop = ((innerHeight - height) / 2) + "px";
-            }
-        }
-        else {
-            app_container.style.marginLeft = ((innerWidth - width) / 2) + "px";
-            app_container.style.marginTop = ((innerHeight - height) / 2) + "px";
-        }
     
     
     
+        //No scale
+        var margin_left = ((innerWidth - width) / 2);
+        margin_left = margin_left > 0 ? margin_left : 0;
+        var margin_top = ((innerHeight - height) / 2);
+        margin_top = margin_top > 0 ? margin_top : 0;
+        app_container.style.marginLeft = margin_left + "px";
+        app_container.style.marginTop = margin_top + "px";
     
         var dpi = 1;
     
