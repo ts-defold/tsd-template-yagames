8b0bf12e52b50b7787755343d8beb6d9bfe6196f
Nathan Bolton <github@thinknathan.com>
2025-10-26 22:40:54 +0000

    chore: upgrade deps (#81)
    * chore: upgrade deps
    
    * fix: update for latest type definitions
    
    * fix: type error

--- src/scripts/score.gui_script.ts
+++ src/scripts/score.gui_script.ts
@@ -22,7 +22,7 @@ interface props {
 		'cursor-1': { 'up-1': node; 'down-1': node; [key: string]: node };
 		'cursor-2': { 'up-2': node; 'down-2': node; [key: string]: node };
 		'cursor-3': { 'up-3': node; 'down-3': node; [key: string]: node };
-		[key: string]: node;
+		[key: string]: node | { [key: string]: node };
 	};
 }
 
@@ -76,7 +76,7 @@ export function update(this: props): void {
 		this.submit = 'submitting';
 
 		//! HTML5 Only
-		const info = sys.get_sys_info() as { system_name: string };
+		const info = sys.get_sys_info();
 		if (info.system_name !== 'HTML5') {
 			offlineScores.set_score(this.score, this.initials.join(''));
 			msg.post('main:/main#script', 'show_fullscreen_adv', {
@@ -147,7 +147,7 @@ export function on_input(this: props, action_id: hash, action: Action): void {
 
 	// Update current letter
 	gui.set_text(
-		this.nodes[`letter-${this.initials_index + 1}`],
+		this.nodes[`letter-${this.initials_index + 1}`] as node,
 		this.letters[this.letters_index],
 	);
 
